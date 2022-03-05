136df0a44618da3f9eb2e35cb20821327c4126ac
souhaila.aouaouri <mhabaieb24@gmail.com>
2022-03-05 14:30:42 +0100

    add modal of post

---
+++ src/app/addpublication/addpublication.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddpublicationComponent } from './addpublication.component';
+
+describe('AddpublicationComponent', () => {
+  let component: AddpublicationComponent;
+  let fixture: ComponentFixture<AddpublicationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AddpublicationComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddpublicationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
